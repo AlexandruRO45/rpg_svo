6eaa0b6bc723ee1826c6c1c2b8f2766a36300834
Alexandru Sava <savaalexandru562@gmail.com>
2025-08-07 12:32:18 +0300

    update install_sys_deps.sh

--- .github/scripts/install_sys_deps.sh
+++ .github/scripts/install_sys_deps.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -e
 
+
+echo "Updating CentOS 7 repository configurations..."
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+echo "Repository configurations updated."
+
 yum update -y
 yum install -y epel-release
+
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/epel*.repo
+sed -i 's|#baseurl=http://download.fedoraproject.org/pub|baseurl=http://archives.fedoraproject.org/pub|g' /etc/yum.repos.d/epel*.repo
+
+
 yum install -y \
     boost-devel \
     opencv-devel \
     eigen3-devel \
-    suitesparse-devel
+    suitesparse-devel \
+    cmake \
+    gcc-c++ \
+    make
